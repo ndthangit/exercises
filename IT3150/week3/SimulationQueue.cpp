613489be0ecb1371001d1b26f334814ca400d696
ndthangit <ndtducthang2004@gmail.com>
2024-10-07 10:50:21 +0700

    update

--- IT3150/week3/SimulationQueue.cpp
+++ IT3150/week3/SimulationQueue.cpp
@@ -1,7 +1,9 @@
 /*
-Perform a sequence of operations over a queue, each element is an integer:
+Perform a sequence of operations over a queue, each element is 
+an integer:
 PUSH v: push a value v into the queue
-POP: remove an element out of the queue and print this element to stdout (print NULL if the queue is empty)
+POP: remove an element out of the queue and print this element 
+to stdout (print NULL if the queue is empty)
 Input
 Each line contains a command (operration) of type
 PUSH  v
@@ -36,3 +38,38 @@ Output
 NULL
 4
 */
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    queue<int> s;
+    string control;
+
+    while (control != "#")
+    {
+        cin >> control;
+        if (control == "PUSH")
+        {
+            int a;
+            cin >> a;
+            s.push(a);
+        }
+        else if (control == "POP")
+        {
+            if (s.empty())
+            {
+                cout << "NULL" << endl;
+            }
+            else
+            {
+                cout << s.front() << endl;
+                s.pop();
+            }
+        }
+    }
+
+    return 0;
+}
